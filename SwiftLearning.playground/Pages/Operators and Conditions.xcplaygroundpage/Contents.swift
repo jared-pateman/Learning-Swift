50fec8c988a8aa145a9fda90d8f5ff2082efe923
Jared Pateman <jared.pateman@outlook.com>
2023-05-27 13:59:09 +0100

    Add operators and conditions page, with starting information on arithmetic operators

---
+++ SwiftLearning.playground/Pages/Operators and Conditions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+// Operators and Conditions
+import UIKit
+
+// Arithmetic Operators
+
+// Operators are mathematical symbols like "+" & "-". Swift has a huge range of them.
+// Performing operations on an int with a double = 2.5 * 2 produces a double.
+
+// Constants to use for different operations.
+let firstValue = 9
+let secondValue = 10
+
+// Addition & Subtraction
+let total = firstValue + secondValue
+let difference = secondValue - firstValue
+
+// Multiplication and Division
+let product = firstValue * secondValue
+let dividedResult = secondValue / firstValue
+
+// Modulus/Remainder
+let thirdValue = 4
+let remainder = secondValue % thirdValue
