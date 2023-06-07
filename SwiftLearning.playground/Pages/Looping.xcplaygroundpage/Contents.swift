590e1301ad6e1889d104de3c513f732422643f13
Jared Pateman <jared.pateman@outlook.com>
2023-06-07 21:04:34 +0100

    Add for loops to Looping page

--- SwiftLearning.playground/Pages/Looping.xcplaygroundpage/Contents.swift
+++ SwiftLearning.playground/Pages/Looping.xcplaygroundpage/Contents.swift
@@ -1,2 +1,31 @@
 // Operators and Conditions
 import UIKit
+
+// There are multiple ways to write loops, underlying mechanism
+// is the same: run code repeatedly until a condition evaluates as false.
+
+
+// For Loops
+
+// For loops are the most common type, can loop over arrays and ranges.
+// Each time the loop goes round it will pull out one item and assign to a constant.
+
+// For loop example:
+let count = 1...10
+
+// Can use a for loop to count to ten:
+for number in count {
+    print("I am counting, and my current number is \(number)")
+}
+
+// Can also iterate through arrays:
+let englishCities = ["London", "Birmingham", "Manchester"]
+for city in englishCities {
+    print("I want to visit \(city)")
+}
+
+// If we don't want to use the constant that you declare in the for loop,
+// we should use an underscore instead, that way Swift doesn't create needless values:
+for _ in 1...5 {
+    print("Hello, World!")
+}
